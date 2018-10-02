ad0c4f72c9b4a8ac55f951fd8bc2cdf00011d2c8
Omm Mishra <41461196+ommmishra@users.noreply.github.com>
2018-10-02 19:22:55 +0530

    Add files via upload

---
+++ BinarySearch.java
@@ -0,0 +1,31 @@
+
+class BinarySearch {
+
+    public static int findBybinarySearch(int[] arr, int n, int key){
+        int lowerBound = 0;
+        int upperBound = n-1;
+        while (true) {
+            int current = (lowerBound+upperBound)/2;
+            if (arr[current]==key)
+                return current+1; //This here is done to return the exact position of the element.
+            else if(lowerBound>current)
+                return -1;
+            else
+                if(arr[current]<key)
+                    lowerBound = current+1;
+                else
+                    upperBound = current -1;
+        }
+        }
+
+
+
+
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[]{1,2,3,4,5,6,7};
+        System.out.println(findBybinarySearch(arr, arr.length, 7));
+
+    }
+}
