39f5c82faefea64a692da523b3d80525c006e9c2
Omm Mishra <41461196+ommmishra@users.noreply.github.com>
2018-10-17 20:13:42 +0000

    Add files via upload

---
+++ SingleLinkedList.java
@@ -0,0 +1,77 @@
+class Link{
+
+    private int data;
+    public Link next;
+
+    public Link(int data){
+        this.data = data;
+    }
+
+    public void display(){
+        System.out.println("Data entered is: "+data);
+    }
+}
+
+class SingleLinkedList {
+
+    private Link first;
+    private Link last;
+    private int n;
+    public SingleLinkedList(){
+        this.first = null;
+        this.last = null;
+        this.n = 0;
+    }
+
+    public Boolean isEmpty(){
+        if(first == null)
+            return true;
+        else
+            return false;
+    }
+
+    public void insertFirst(int data){
+        Link NewLink = new Link(data);
+
+        if(isEmpty())
+            last = NewLink;
+        NewLink.next = first;
+        first = NewLink;
+    }
+
+    public void insertLast(int data){
+        Link NewLink = new Link(data);
+
+        if(isEmpty())
+            first = NewLink;
+        else {
+            Link current = first;
+            while (current.next!=null){
+                current = current.next;
+            }
+            current.next = NewLink;
+            last = NewLink;
+        }
+    }
+    public void diplay(){
+        Link current = first;
+        while(current != null){
+            current.display();
+            System.out.println();
+            current = current.next;
+        }
+    }
+}
+class SingleLinkedListApp{
+    public static void main(String[] args) {
+        SingleLinkedList LinkedList = new SingleLinkedList();
+
+        LinkedList.insertFirst(55);
+        LinkedList.insertFirst(88);
+        LinkedList.insertLast(789);
+        LinkedList.insertLast(44);
+
+        LinkedList.diplay();
+    }
+}
+
